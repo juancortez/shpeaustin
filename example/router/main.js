09a4934e76902353fa1a249dae463cc958a0a1f2
Juan Cortez <JCortez@us.ibm.com>
2015-09-15 17:38:15 -0500

    Added new drinks.ejs route.

--- example/router/main.js
+++ example/router/main.js
@@ -1,7 +1,7 @@
 // module.exports exposes functions that we want to use in a different file
 module.exports = function(app){
 	app.get('/', function(req, res){
-		res.render('home.html');
+		res.render('index.html');
 	});
 	app.get('/about', function(req, res){
 		res.render('about.html');
@@ -10,4 +10,17 @@ module.exports = function(app){
 		res.render('contact.html');
 	});
 
+	app.get('/drinks', function(req, res){
+		var drinks = [
+				{ name: 'Bloody mary', drunkness: 3 },
+				{ name: 'Martini', drunkness: 5 },
+				{ name: 'Scotch', drunkness: 10 }
+		];
+		var tagline = "Insert a tagline here.";
+
+		res.render('drinks.ejs', {
+			drinks: drinks,
+			tagline: tagline
+		});
+	});
 }
