800581ac04a92d1d01253482720e98a1d172d4d5
Juan Cortez <JCortez@us.ibm.com>
2015-09-15 17:46:15 -0500

    Added error handling to HTML page.

--- example/router/main.js
+++ example/router/main.js
@@ -1,11 +1,14 @@
 // module.exports exposes functions that we want to use in a different file
 module.exports = function(app){
+	
 	app.get('/', function(req, res){
 		res.render('index.html');
 	});
+
 	app.get('/about', function(req, res){
 		res.render('about.html');
 	});
+
 	app.get('/contact', function(req, res){
 		res.render('contact.html');
 	});
@@ -22,5 +25,9 @@ module.exports = function(app){
 			drinks: drinks,
 			tagline: tagline
 		});
+	});
+
+	app.get('*', function(req, res){
+  		res.status(400).send({ error: 'HTML Error 404: Not Found!' });
 	});
 }
