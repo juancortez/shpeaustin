157e72efcda43b48dc294f0bc03e342e6783d972
Juan Cortez <JCortez@us.ibm.com>
2016-08-20 19:29:06 -0500

    Refactored backend code and added botkit.

--- app.js
+++ app.js
@@ -11,17 +11,26 @@ var express = require('express'),
     redisCredentials = privateCredentials.redis.credentials,
     redis_connect = require("./redis/redis.js"),
     socket_connect = require("./socket/socket.js"),
-    socket = require('socket.io');
+    socket = require('socket.io'),
+    config = require('config');
 
-
-// Connect to Redis Database
+/************************************************************************************************************
+*                                   Redis Database Connection
+************************************************************************************************************/
 var client = redis.createClient(redisCredentials.port, redisCredentials.hostname, {no_ready_check: true});
+app.set('redis', client); 
 client.auth(redisCredentials.password, function (err) {
     if (err){
     	console.error(err);
     }
 });
+client.on('connect', function() {
+    redis_connect.onRedisConnection(client, redis);
+});
 
+/************************************************************************************************************
+*                                  Express App Configuration
+************************************************************************************************************/
 app.use(compression()); //use compression 
 app.use(express.static(__dirname + '/public', { maxAge: 604800000 /* 7d */ })); // 1d = 86400000
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -29,12 +38,15 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
 app.use(favicon(__dirname + '/public/assets/shpe_austin_icon.png'));
-require('./router/main')(app, client, privateCredentials); // adds the main.js file to send response to browser
+require('./router/main')(app, client); // adds the main.js file to send response to browser
 app.set('views', __dirname + '/views'); // defines where our HTML files are placed
 app.set('view engine', 'ejs'); // used for HTML rendering
 app.engine('html', require('ejs').__express); // rendering HTML files through EJS
 
 
+/************************************************************************************************************
+*                                  BlueMix Configurations
+************************************************************************************************************/
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -47,10 +59,30 @@ var server = app.listen(appEnv.port, '0.0.0.0', function() {
     console.log("Server starting on " + appEnv.url);
 });
 
-// web sockets
+/************************************************************************************************************
+*                                  Web Socket Configuration
+************************************************************************************************************/
 var io = socket.listen(server);
 socket_connect.initiateSocket(io, client);
 
-client.on('connect', function() {
-    redis_connect.onRedisConnection(client, redis);
+/************************************************************************************************************
+*                                  BotKit Configuration
+* SHPE-Austin Slack Integrations: https://shpeaustin.slack.com/apps/manage/custom-integrations
+************************************************************************************************************/
+var Botkit = require('botkit'),
+    slackConfig = config.slack;
+
+var controller = Botkit.slackbot({
+  debug: false
 });
+
+// connect the bot to a stream of messages
+var bot = controller.spawn({
+  token: slackConfig.botToken,
+  incoming_webhook:{
+    url: slackConfig.subscribeRequestWebHook
+  }
+}).startRTM();
+
+app.set('bot', bot); 
+require('./slackbot/slack.js')(controller, client); // Listen to different requests
