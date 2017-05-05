47aff2dc77e3741f59fe1011e96e96b35d7860ff
Juan Cortez <JCortez@us.ibm.com>
2017-05-05 14:39:58 -0500

    Beginning progress on contact page.

--- src/app/contact.component.ts
+++ src/app/contact.component.ts
@@ -8,4 +8,34 @@ import { Component } from "@angular/core";
 
 
 export class ContactComponent {
+	categories:any = [
+		'General',
+		'Athletics',
+		'Corporate',
+		'Events',
+		'Mentorship',
+		'Regional/National Conferences',
+		'Scholarship',
+		'Website',
+		'Other'
+	];
+	formName: string = "";
+	formEmail: string = "";
+	formPhone: string = "";
+	formCategory: string = this.categories[0];
+	formMessage: string = "";
+	submitted = false;
+
+	onCategoryChange(category: string){
+		this.formCategory = category;
+	}
+
+	onMessageChange(message: string):void{
+		this.formMessage = message;
+	}
+
+	onSubmit():void {
+		console.log(this);
+		this.submitted = true;
+	}
 }
