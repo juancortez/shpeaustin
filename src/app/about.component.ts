c171a2ebd33659605bc3e0e0aae9a071eb1564da
Juan Cortez <JCortez@us.ibm.com>
2017-05-11 14:44:13 -0500

    Cleaned up static content from about page.

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 
+import { SHPE_MISSION, SHPE_VISION, SHPE_HISTORY, SHPE_AUSTIN_HISTORY, SHPE_BYLAWS } from './static/text.template';
+
 @Component({
   selector: "about",
   templateUrl: "./templates/about.component.html",
@@ -8,4 +10,9 @@ import { Component } from "@angular/core";
 
 
 export class AboutComponent {
+	mission: string = SHPE_MISSION;
+	vision: string = SHPE_VISION;
+	history: string = SHPE_HISTORY;
+	austinHistory: string = SHPE_AUSTIN_HISTORY;
+	bylaws: string = SHPE_BYLAWS;
 }
