3afba228e7ecce136623d34d83b5d53647600f9a
Juan Cortez <JCortez@us.ibm.com>
2017-05-17 15:10:23 -0500

    Cleaning up old files.

--- client/app/static/membership-info.template.ts
+++ client/app/static/membership-info.template.ts
@@ -1,37 +1,37 @@
 export const DESCRIPTIONS: Object = {  
    "mentoring":[  
-      "Our chapter has partnered with both The University of Texas at Austin Chapter and SHPE Jr. Chapters to create a mentorship program. Professionals in",
-      " the SHPE Austin chapter are matched with college students with similar career choices and provide mentorship and career advice."
+      `Our chapter has partnered with both The University of Texas at Austin Chapter and SHPE Jr. Chapters to create a mentorship program. 
+      Professionals in the SHPE Austin chapter are matched with college students with similar career choices and provide mentorship and career advice.`
    ],
    "community_outreach":[  
-      "Our chapter prides itself with community outreach and hosts various activities with The University of Texas at Austin chapter.",
-      "Some of the annual events that SHPE Austin hosts are Noche de Ciencias and Introduce a Girl to Engineering Day. Each month, our ",
-      "Community Service Director hosts a community service event, ranging from mentoring children, cleaning parks, and participating in toy drives."
+      `Our chapter prides itself with community outreach and hosts various activities with The University of Texas at Austin chapter.
+      Some of the annual events that SHPE Austin hosts are Noche de Ciencias and Introduce a Girl to Engineering Day. Each month, our
+      Community Service Director hosts a community service event, ranging from mentoring children, cleaning parks, and participating in toy drives.`
    ],
    "leadership_development":[  
-      "Throughout the year, officer positions become available and members are encouraged to apply for these positions. If you are interested in learning more about",
-      " how the officer board functions or want to enhance your leadership skills as a professional, come around to our events so we can meet you!"
+      `Throughout the year, officer positions become available and members are encouraged to apply for these positions. If you are interested in learning more about
+       how the officer board functions or want to enhance your leadership skills as a professional, come around to our events so we can meet you!`
    ],
    "professional_development":[  
-      "As a professional, it is important to continue developing your skills so the SHPE Austin chapter provides members with workshops where members can ",
-      "become better public speakers, find ways to enhance their brands, and teach how to mentor students."
+      `As a professional, it is important to continue developing your skills so the SHPE Austin chapter provides members with workshops where members can 
+      become better public speakers, find ways to enhance their brands, and teach how to mentor students.`
    ],
    "chapter_development":[  
-      "When becoming a professional, it can be daunting to move to a new city. The SHPE Austin chapter makes the transition a little easier by ",
-      " hosting socials and events around Austin. Check out our calendar and join us at some of our events to learn more about what Austin, TX has to offer."
+      `When becoming a professional, it can be daunting to move to a new city. The SHPE Austin chapter makes the transition a little easier by 
+       hosting socials and events around Austin. Check out our calendar and join us at some of our events to learn more about what Austin, TX has to offer.`
    ],
    "sports":[  
-      "Austin, TX is well known for being an outdoors city and there are various leagues that SHPE Austin participates in. In past years, our chapter has been part of ",
-      "many leagues, including soccer, volleyball, softball, and football."
+      `Austin, TX is well known for being an outdoors city and there are various leagues that SHPE Austin participates in. In past years, our chapter has been part of 
+      many leagues, including soccer, volleyball, softball, and football.`
    ],
    "national_conferences":[  
-      "The Society of Hispanic Professional Engineers National Organization hosts a yearly natinal conference and brings together all ",
-      "the chapters across the world, including SHPE Jr. Chapters, University Chapters, and Professional Chapters. The National Conference is usually held in November and ",
-      "offers networking opportunities and professional workshops for all members of any age."
+      `The Society of Hispanic Professional Engineers National Organization hosts a yearly natinal conference and brings together all
+      the chapters across the world, including SHPE Jr. Chapters, University Chapters, and Professional Chapters. The National Conference is usually held in November and
+      offers networking opportunities and professional workshops for all members of any age.`
    ],
    "regional_conferences":[  
-      "Every year, one University chapter from each region is chosen to host a Regional conference. The Regional Conference offers many of the same benefits and opportunities ",
-      "as the national conference, but at a smaller level. The Regional Conference is usually held during the month of March and includes activities and events available for all ",
-      "levels, including SHPE Jr, University, and Professional."
+      `Every year, one University chapter from each region is chosen to host a Regional conference. The Regional Conference offers many of the same benefits and opportunities 
+      as the national conference, but at a smaller level. The Regional Conference is usually held during the month of March and includes activities and events available for all 
+      levels, including SHPE Jr, University, and Professional.`
    ]
 };
