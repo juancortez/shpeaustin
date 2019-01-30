49bfac157d29900a9e75b321e2201dbe43e4a92b
Juan Cortez <jucorte@microsoft.com>
2019-01-30 15:26:19 -0800

    Adding simple input lookup for logs

--- client/app/react/components/Log.tsx
+++ client/app/react/components/Log.tsx
@@ -5,20 +5,54 @@ interface ILogProps {
     logs: ILog[];
 }
 
-export class Log extends React.Component<ILogProps, {}> {
+interface ILogState {
+    search: string;
+}
+
+export class Log extends React.Component<ILogProps, ILogState> {
+    private _idInputId: string = "search-logs";
+
+    constructor(props) {
+        super(props);
+
+        this._handleInputChange = this._handleInputChange.bind(this);
+    }
+
+    state = {
+        search: ''
+    };
+
     public render() {
         return (
-            <table className="shpe-logs">
-                <thead>
-                    <tr>
-                        { this._createTableHeadings() }
-                    </tr>
-                </thead>
-                <tbody>
-                    { this._createTableBody() }
-                </tbody>
-            </table>
+            <>
+                <div className="search-log-container">
+                    <input
+                        id={this._idInputId}
+                        placeholder="Search logs..."
+                        value={this.state.search}
+                        onChange={this._handleInputChange}
+                    />
+                </div>
+                <table className="shpe-logs">
+                    <thead>
+                        <tr>
+                            { this._createTableHeadings() }
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this._createTableBody() }
+                    </tbody>
+                </table>
+            </>
         )
+    }
+
+    private _handleInputChange(evt) {
+        const search = evt.target.value;
+
+        this.setState({
+            search
+        });
     }
 
     private _createTableHeadings() {
@@ -32,9 +66,15 @@ export class Log extends React.Component<ILogProps, {}> {
 
     private _createTableBody() {
         const logs = this.props.logs;
+        const { search } = this.state;
 
         return logs.sort((a, b) => {
             return -(a.date - b.date);
+        }).filter(currentLog => {
+            const logToLower = currentLog.log.toLocaleLowerCase();
+            const searchTerm = search.toLocaleLowerCase();
+
+            return logToLower.indexOf(searchTerm) >= 0;
         }).map((currentLog, index) => {
             const { date, log, severity } = currentLog;
 
